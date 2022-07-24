ec538486fb0de8eb28f3dd6b095b219b7ff5784c
takumi-1016 <tn1016recruit@gmail.com>
2022-07-25 00:38:01 +0900

    ナビメニューのjsを追加

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -3,8 +3,7 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    Popper: ['popper.js', 'default']
+    $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery'
 }))
 module.exports = environment
